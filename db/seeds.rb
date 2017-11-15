070a52fdfa575f256229db314fce3a4754bb120f
MansoorZaman <manisureal@outlook.com>
2017-11-15 13:01:03 +0000

    Completed the index page for Cars

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#Create a User first
+
+new_user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456")
+puts 'User created'
+
+#Create 10 cars then
+
+9.times do |n|
+  Car.create(make: "Mercedes #{n}", model: "CLS 63", seats: "5", engine_size: "6", transmission: "auto", color: "Black", reg_year: "#{n}",  fuel_type: "Petrol", user: new_user)
+end
+
+puts "created 10 cars"
